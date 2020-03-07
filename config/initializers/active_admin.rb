56a203b5b86dcc1ed713627300cb583f12f6aea8
Michael Schiller <michael@agil-verwirklichen.de>
2020-03-07 11:46:47 +0100

    design change v1

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -9,14 +9,14 @@ ActiveAdmin.setup do |config|
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
-  config.site_title_link = '/'
+  config.site_title_link = "/"
 
   # Set an optional image to be displayed for the header
   # instead of a string (overrides :site_title)
   #
   # Note: Aim for an image that's 21px high so it fits in the header.
   #
-  # config.site_title_image = 'media/images/logo.png'
+  # config.site_title_image = 'media/images/logo.jpg'
 
   # == Default Namespace
   #
@@ -112,7 +112,7 @@ ActiveAdmin.setup do |config|
   # roots for each namespace.
   #
   # Default:
-  config.root_to = 'dashboard#index'
+  config.root_to = "dashboard#index"
 
   # == Admin Comments
   #
@@ -291,7 +291,7 @@ ActiveAdmin.setup do |config|
   #
   # config.order_clause = MyOrderClause
 
-  meta_tags_options = { viewport: 'width=device-width, initial-scale=1' }
+  meta_tags_options = { viewport: "width=device-width, initial-scale=1" }
   config.meta_tags = meta_tags_options
   config.meta_tags_for_logged_out_pages = meta_tags_options
 end
