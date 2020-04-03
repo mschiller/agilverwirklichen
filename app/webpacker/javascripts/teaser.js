8da93b43f94456b41a38118a7a9d4f6858841918
Michael Schiller <michael@agil-verwirklichen.de>
2020-04-03 07:47:50 +0200

    improve responsive teaser design

--- app/webpacker/javascripts/teaser.js
+++ app/webpacker/javascripts/teaser.js
@@ -3,10 +3,10 @@ import 'bxslider/dist/jquery.bxslider';
 $(function () {
   $('.bxslider').bxSlider({
     auto: true,
-    pause: 6000,
+    pause: 5000,
     autoHover: false,
     minSlides: 2,
-    maxSlides: 5,
+    maxSlides: 20,
     randomStart: true,
     infiniteLoop: true,
     randomStart: true,
@@ -14,38 +14,30 @@ $(function () {
     mode: 'horizontal',
     //adaptiveHeight: true,
     //preloadImages: 'all',
-    hideControlOnEnd: true,
-
-    onSliderLoad: function () {
-      setTimeout(function () {
-        $('#teaser').css('visibility', 'visible');
-      }, 20);
-    }
+    hideControlOnEnd: false,
   });
 });
 
-var xSeconds = 3000; // 2 second
+// var xSeconds = 3000; // 2 second
 
+// function resize() {
+//   var height = (window.innerHeight - 185) + "px"
+//   var js_full_teaser = $("#js_full_teaser")
+//   if (typeof (js_full_teaser) != 'undefined' && js_full_teaser != null) {
+//     //js_full_teaser.css("height", height);
+//     //$(".bxslider").css("height", height);
+//     //$(".bx-clone").css("height", height);
+//   }
+// }
 
-function resize() {
-  var heights = window.innerHeight;
-  var js_full_teaser = $("#js_full_teaser")
-  if (typeof (js_full_teaser) != 'undefined' && js_full_teaser != null) {
-    js_full_teaser.css("height", (heights - 185) + "px");
+// $(document).ready(function () {
+//   resize();
 
-    $(".bxslider").css("height", heights / 2);
-    $(".bx-clone").css("height", heights / 2);
-  }
-}
+//   setTimeout(function () {
+//     $('#js_fade_header').fadeIn('slow');
+//   }, xSeconds);
+// });
 
-$(document).ready(function () {
-  resize();
-
-  setTimeout(function () {
-    $('#js_fade_header').fadeIn('slow');
-  }, xSeconds);
-});
-
-window.onresize = function () {
-  resize();
-};
+// window.onresize = function () {
+//   resize();
+// };
