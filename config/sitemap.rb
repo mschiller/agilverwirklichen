09806a18ca23814d1a5f3c3508eb95d842445abe
Michael Schiller <michael@agil-verwirklichen.de>
2020-04-05 20:24:07 +0200

    configure sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,12 +1,19 @@
 host Figaro.env.domain
-protocol Figaro.env.protocol
 
 sitemap :site do
 
   # default per_page is 50.000 which is the specified maximum.
   # per_page 10
 
-  url root_url, last_mod: DateTime.now, change_freq: 'daily', priority: 1
+  url root_url, last_mod: DateTime.now, change_freq: "weekly", priority: 0.9
+
+  Comfy::Blog::Post.published.each do |post|
+    url post.url, last_mod: post.updated_at, change_freq: "daily", priority: 1
+  end
+
+  Comfy::Cms::Site.first.pages.root.children.published.each do |page|
+    url page.url(relative: true), last_mod: page.updated_at, change_freq: "monthly", priority: 0.5
+  end
 end
 
 ping_with "https://#{host}/sitemap.xml"
