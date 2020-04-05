014ebcb12e2b9a36becf67857751ed576ac8da4e
Michael Schiller <michael@agil-verwirklichen.de>
2020-04-05 09:46:18 +0200

    add edirect old urls

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,15 @@ Rails.application.routes.draw do
 
   ActiveAdmin.routes(self)
 
+  # redirects old => new
+  get "/blog-post/agilitaet-geschwindigkeit/" => redirect("/blog/2018/10/agilitaet-geschwindigkeit")
+  get "/blog-post/gedanken-zu-3-gruende-warum-scrum-in-der-politik-nichts-zu-suchen-hat/" => redirect("/blog/2018/10/gedanken-zu-3-gruende-warum-scrum-in-der-politik-nichts-zu-suchen-hat")
+  get "/blog-post/teambuilding-mit-market-of-skills/" => redirect("/blog/2018/6/teambuilding-mit-market-of-skills-workshop")
+  get "/blog-post/gruendung-mit-hilfe-der-arbeitsagentur/" => redirect("/blog/2018/1/gruendung-mit-hilfe-der-arbeitsagentur")
+  get "/blog-post/scrum-3-0-im-einsatz/" => redirect("/blog/2018/1/scrum-30-im-einsatz")
+  get "/blog-post/the-next-big-thing-autonomous-self-organized-units-2/" => redirect("/blog/2017/10/the-next-big-thing-autonomous-self-organized-units-2")
+  get "/blog-post/the-next-big-thing-autonomous-self-organized-units-1/" => redirect("/blog/2017/9/the-next-big-thing-autonomous-self-organized-units-1")
+
   get "/news/feed", to: "feeds#news", defaults: { format: "atom" }, as: "newsfeed"
 
   get "/kontakt", :to => "contact#new", :as => "contact"
