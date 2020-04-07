06304d9d6442237159c1344010861df3ecc74bd3
Michael <michael@agil-verwirklichen.de>
2020-04-07 10:49:29 +0200

    schedule getting Proven Expert image at every night

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,16 +5,19 @@
 
 env :PATH, "#{ENV["PATH"]}:#{Dir.pwd}/bin"
 
-set :output, {:error => "#{Dir.pwd}/log/cron_error_log.log", :standard => "#{Dir.pwd}/log/cron_log.log"}
+set :output, { :error => "#{Dir.pwd}/log/cron_error_log.log", :standard => "#{Dir.pwd}/log/cron_log.log" }
 
 case @environment
-  when 'production'
+when "production"
+  every 1.day, at: "6:00 am", :roles => [:app] do
+    rake "sitemap:generate"
+  end
 
-    every 1.day, at: '6:00 am', :roles => [:app] do
-      rake 'sitemap:generate'
-    end
+  every 1.day, at: "4:00 am", :roles => [:app] do
+    command "cd #{Dir.pwd}/public/system/b8545e1a72a8ca899b491ea2b6e0/; wget https://images.provenexpert.com/ba/01/b8545e1a72a8ca899b491ea2b6e0/widget_square_165_de_1.png"
+  end
 
-    every 1.day, at: '5:00 am', :roles => [:app] do
-      #command 'backup perform --trigger xxx_backup'
-    end
+  every 1.day, at: "5:00 am", :roles => [:app] do
+    #command 'backup perform --trigger xxx_backup'
+  end
 end
