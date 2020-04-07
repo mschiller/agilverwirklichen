37c49f6e3773933c9bd7e1520a4428f33bc96829
Michael <michael@agil-verwirklichen.de>
2020-04-07 14:15:28 +0200

    reset to default content_security_policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,19 +4,19 @@
 
 Rails.application.config.content_security_policy do |p|
   p.default_src :self, :https
-  p.font_src :self, :https, :data
-  p.img_src :self, :https, :data, :unsafe_inline, :unsafe_eval #, "https://images.provenexpert.com", "https://provenexpert.com", "https://www.google-analytics.com"
-  p.object_src :none
-  p.script_src :self, :https, :unsafe_inline, :unsafe_eval #, "https://calendly.com", "https://assets.calendly.com", "https://www.google-analytics.com"
-  p.style_src :self, :https, :unsafe_inline
+  p.font_src    :self, :https, :data
+  p.img_src     :self, :https, :data
+  p.object_src  :none
+  p.script_src  :self, :https, :unsafe_eval, :unsafe_inline
+  p.style_src   :self, :https, :unsafe_inline
 
   # Specify URI for violation reports
   # p.report_uri "/csp-violation-report-endpoint"
 
-  p.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
+  p.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035' if Rails.env.development?
 end
 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-#Rails.application.config.content_security_policy_report_only = true
+# Rails.application.config.content_security_policy_report_only = true
