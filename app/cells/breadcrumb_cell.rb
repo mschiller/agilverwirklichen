3146f229d7df7f922a3b4a0e1660d589fcce5f5a
Michael <michael@agil-verwirklichen.de>
2020-04-14 16:48:47 +0200

    no breadcrumbs for mobile

--- app/cells/breadcrumb_cell.rb
+++ app/cells/breadcrumb_cell.rb
@@ -5,7 +5,7 @@ class BreadcrumbCell < Cell::ViewModel
   include Escaped
 
   cache :show, expires_in: ::Rails.configuration.cache_default_time_in_minutes.to_i.minutes do
-    ["breadcrumb_cell", request.path, get_last_updated_at_for(::Comfy::Cms::Page)]
+    ["breadcrumb_cell", is_mobile_device?, request.path, get_last_updated_at_for(::Comfy::Cms::Page)]
   end
 
   def show(options = {})
