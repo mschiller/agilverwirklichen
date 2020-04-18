38e03d08253fa09fc6e252823790af7ad228dfd1
Michael <michael@agil-verwirklichen.de>
2020-04-18 15:07:38 +0200

    rails 6 update

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,12 @@
-# Load the rails application
-require File.expand_path('../application', __FILE__)
+# Load the Rails application.
+require_relative 'application'
 
 if Rails.env.development?
-  projectrc = File.expand_path('../../.railsrc', __FILE__)
+  projectrc = File.expand_path("../../.railsrc", __FILE__)
   if File.exist?(projectrc)
     load projectrc
   end
 end
 
-# Initialize the rails application
-Framework::Application.initialize!
+# Initialize the Rails application.
+Rails.application.initialize!
