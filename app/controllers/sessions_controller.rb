5d1eeddc8ecc80b7897a9203e99072fedbcb7c27
Michael <michael@agil-verwirklichen.de>
2020-04-20 23:09:46 +0200

    set classroom_referer for session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < Devise::SessionsController
 
     if session[:classroom_referer]
       @default_user_email = Figaro.env.default_class_email_address
-      session[:classroom_referer] = nil
     end
 
     store_location_for(:user, request.referer)
