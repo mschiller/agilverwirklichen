1d2e2120ba11c13ecb806db255a2bde8a52321bb
Michael <michael@agil-verwirklichen.de>
2020-04-26 09:23:03 +0200

    fix sitemap urls

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -8,11 +8,11 @@ sitemap :site do
   url root_url, last_mod: DateTime.now, change_freq: "weekly", priority: 0.9
 
   Comfy::Blog::Post.published.each do |post|
-    url post.url, last_mod: post.updated_at, change_freq: "daily", priority: 1
+    url ["https:", post.url].join(), last_mod: post.updated_at, change_freq: "daily", priority: 1
   end
 
   Comfy::Cms::Site.first.pages.root.children.published.each do |page|
-    url page.url(relative: true), last_mod: page.updated_at, change_freq: "monthly", priority: 0.5
+    url ['https:', page.url].join(), last_mod: page.updated_at, change_freq: "monthly", priority: 0.5
   end
 end
 
